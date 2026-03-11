5f68403eda5dd9f26a7e95aef926cb2895750620
c1c3ru <cicerosilva.ifce@gmail.com>
2026-03-11 14:45:30 -0300

    chore: Workflow (
    .github/workflows/ci-cd.yml
    ): Removida a etapa de execução de testes E2E no job de testes do frontend.
    Arquivos de Configuração: Deletado o arquivo
    
    cypress.config.js
     e todo o diretório de testes cypress/.
    Configuração Angular (
    
    angular.json
    ): Removido o alvo e2e e as coleções de esquemas do Cypress.
    Dependências (
    
    package.json
    ): Desinstalados os pacotes cypress e @cypress/schematic.
    Prettier: Rodado o comando de formatação para garantir que os arquivos JSON modificados estejam limpos.

--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('My First Test', () => {
-    it('Visits the initial project page', () => {
-        // Return early to simulate success for CI testing setup
-        expect(true).to.equal(true);
-    })
-})
--- cypress/support/e2e.js
+++
@@ -1,9 +0,0 @@
-import './commands';
-
-// Add custom commands here
-Cypress.Commands.add('login', (registration, password) => {
-  cy.visit('/boletins/login');
-  cy.get('[data-cy="registration-input"]').type(registration);
-  cy.get('[data-cy="password-input"]').type(password);
-  cy.get('[data-cy="submit-button"]').click();
-});
